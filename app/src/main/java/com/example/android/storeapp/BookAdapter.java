8adafb48611bd2e73937557be270a48465062223
Noemi Balazs <balazsnoe@gmail.com>
2018-01-15 19:17:40 +0200

    Second commit

--- app/src/main/java/com/example/android/storeapp/BookAdapter.java
+++ app/src/main/java/com/example/android/storeapp/BookAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.storeapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -13,13 +15,14 @@ import android.widget.CursorAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.android.storeapp.data.BookContract.BookEntry;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-;
+
 
 /**
  * Created by Noemi on 1/11/2018.
@@ -37,28 +40,41 @@ public class BookAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
 
         TextView nameView = view.findViewById(R.id.adapter_name);
         TextView authorView = view.findViewById(R.id.adapter_author);
-        TextView numberView = view.findViewById(R.id.adapter_number);
+        final TextView numberView = view.findViewById(R.id.adapter_number);
         ImageView imageView = view.findViewById(R.id.adapter_image);
 
         int nameIndex = cursor.getColumnIndex(BookEntry.BOOK_COLUMN_TITLE);
         int authorIndex = cursor.getColumnIndex(BookEntry.BOOK_COLUMN_AUTHOR);
-        int quantityIndex = cursor.getColumnIndex(BookEntry.BOOK_COLUMN_QUANTITY);
+        final int quantityIndex = cursor.getColumnIndex(BookEntry.BOOK_COLUMN_QUANTITY);
         int imageIndex = cursor.getColumnIndex(BookEntry.BOOK_COLUMN_IMAGE);
 
         String name = cursor.getString(nameIndex);
         String author = cursor.getString(authorIndex);
         int quantity = cursor.getInt(quantityIndex);
-
         int image = cursor.getInt(imageIndex);
 
         nameView.setText(name);
         authorView.setText(author);
         numberView.setText(Integer.toString(quantity));
         imageView.setImageResource(image);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int quantity = cursor.getInt(quantityIndex);
+                if (quantity == 0){
+                    Toast.makeText(context, "No more in stock. Please makes some new order!", Toast.LENGTH_SHORT).show();
+                }else {
+                    quantity = quantity - 1;
+                    numberView.setText(Integer.toString(quantity));
+
+                }
+            }
+        });
         
 
 
